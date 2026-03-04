95bec4ef16218ae05429198dac7c08fa50d79f12
iamjaypatelon-ux <iamjaypatelon@gmail.com>
2026-03-04 10:55:09 +0530

    Create createOrder.js

---
+++ api/createOrder.js
@@ -0,0 +1,24 @@
+import Razorpay from "razorpay";
+
+export default async function handler(req, res) {
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+const options = {
+  amount: 1000, // ₹10 test
+  currency: "INR",
+  receipt: "car_booking"
+};
+
+const order = await razorpay.orders.create(options);
+
+res.status(200).json({
+  order_id: order.id,
+  amount: order.amount,
+  key: process.env.RAZORPAY_KEY_ID
+});
+
+}
